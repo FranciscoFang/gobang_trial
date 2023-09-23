d549218d575acf8c7f4514c617c8b101d9bdad10
liquid <fstopgun@gmail.com>
2023-09-23 23:25:10 +0800

    -- trash

--- generating_model.py
+++ generating_model.py
@@ -1,6 +1,6 @@
 import numpy as np
 from board_check import show_board, print_board, check_win_all, compare_board, combine_board, divide_board
-from moves import machine_1, machine_2
+from moves import machine_1, machine_2, machine_1_random, machine_2_random
 from make_models import make_models
     
 def ML_fit_1(allgame, allaction):
@@ -20,7 +20,8 @@ def ML_fit_2(allgame, allaction):
 model1, model2 = make_models()
 
 # if __name__ == '__main__':
-epochs = 1 #as your wish
+logs = open('logs.txt', 'a')
+epochs = 100 #as your wish
 black_wins = 0
 white_wins = 0
 draw_game = 0
@@ -39,58 +40,63 @@ while i<epochs:
     black_play = True
     steps_count = 0
     while check_win_all(board) == 0:
-        if len(sum_allgame_black) > 100:
+        if len(sum_allgame_black) > 1000:
             sum_allgame_black.pop(0)
             sum_allaction_black.pop(0)
-            print("删除一个黑方旧样本")
-        if len(sum_allgame_white) > 100:
+            # print("删除一个黑方旧样本")
+        if len(sum_allgame_white) > 1000:
             sum_allgame_white.pop(0)
             sum_allaction_white.pop(0)
-            print("删除一个白方旧样本")
+            # print("删除一个白方旧样本")
         if check_win_all(board) != 0: break
         print ("到黑方了")
         print (steps_count)
         while check_win_all(board) == 0 and black_play == True:
-            return_board = machine_1(board, model1)
+            if i<=10 : #first x random games
+                return_board_black = machine_1_random(board)
+            else:
+                return_board_black = machine_1(board, model1)
+            if return_board_black == [] or ''.join(str(return_board_black)).find('1') == -1 : print("检查是否在下子")
             allgame_black += [divide_board(board)]
-            allaction_black += [return_board]
-            board = combine_board(board, return_board)
-            # print_board(show_board(board))
+            allaction_black += [return_board_black]
+            board = combine_board(board, return_board_black)
             steps_count += 1
             black_play = False
-            # if check_win_all(board) != 0: break
-            # if black_play == 0: break
             break
         if check_win_all(board) != 0: break
+        if steps_count >= 225: break
         print ("到白方了")
         print (steps_count)
         while check_win_all(board) == 0 and black_play == False:
-            return_board = machine_2(board, model2)
+            if i<=10 : #first x random games
+                return_board_white = machine_2_random(board)
+            else:
+                return_board_white = machine_2(board, model1)
             allgame_white += [divide_board(board)]
-            allaction_white += [return_board]
-            board = combine_board(board, return_board)
-            # print_board(show_board(board))
+            allaction_white += [return_board_white]
+            board = combine_board(board, return_board_white)
+            
             steps_count += 1
             black_play = True
-            # if check_win_all(board) != 0: break
-            # if black_play == True: break
             break
         if check_win_all(board) != 0: break
+        if steps_count >= 225: break
     if check_win_all(board) == 1:
         sum_allgame_black += allgame_black
         sum_allaction_black += allaction_black
         black_wins += 1
-        print ("黑赢+1")
+        print ("黑赢+1", "黑AI获胜次数：", black_wins, "白AI获胜次数：", white_wins)
         ML_fit_1(np.asarray(sum_allgame_black), np.asarray(sum_allaction_black))
     if check_win_all(board) == -1:
         sum_allgame_white += allgame_white
         sum_allaction_white += allaction_white
         white_wins += 1
-        print ("白赢+1")
+        print ("白赢+1", "黑AI获胜次数：", black_wins, "白AI获胜次数：", white_wins)
         ML_fit_2(np.asarray(sum_allgame_white), np.asarray(sum_allaction_white))
     if  check_win_all(board) == 0 and steps_count == 225:
         draw_game += 1
         print ("平局+1")
-    if steps_count == 225:
-        break
+    if i%50 == 0:
+        print ("局目总数：", i, "黑AI获胜次数：", black_wins, "白AI获胜次数：", white_wins, "平局次数：", draw_game, "", file=logs)
+logs.close()
 print("黑AI获胜次数：",black_wins,"白AI获胜次数：",white_wins,"平局次数：",draw_game)
--- moves.py
+++ moves.py
@@ -1,34 +1,67 @@
 import numpy as np
 import random
-from board_check import compare_board, print_board, check_win_all, divide_board
+from board_check import show_board, compare_board, print_board, check_win_all, divide_board
+from forbidden import long_forbidden, double_4, double_3
+import copy
+import time
 
 def make_answer_board_black(argmax_index):
     x = (argmax_index)//15
     y = (argmax_index)%15
     answer_board = [[0]*15 for i in range(15)]
     answer_board[x][y] = 1
-    # print(answer_board)
-    return answer_board
+    return answer_board, x, y
 
 def make_answer_board_white(argmax_index):
     x = (argmax_index)//15
     y = (argmax_index)%15
     answer_board = [[0]*15 for i in range(15)]
     answer_board[x][y] = -1
-    # print(answer_board)
     return answer_board
 
-def find_max_values_black(prediction, board):
+def find_max_values_black(prediction, board, onestep):
     index_sort = np.argsort(np.array(prediction[0]).flatten())
     i = -1
     while True:
         # print ("will select ", i, "-th, index is ", index_sort[i],", value is ", np.array(prediction[0]).flatten()[index_sort[i]], "")
-        answer_board = make_answer_board_black(index_sort[i])
-        if compare_board(answer_board, board) == True:
-            break
-        else:
+        answer_board, x, y, = make_answer_board_black(index_sort[i])
+        if compare_board(answer_board, board) == False:
             i -= 1
             continue
+        else:
+            onestep[x][y] = 1
+            show_next = show_board(onestep)
+            if long_forbidden(show_next) == True:
+                print ("计算评估阶段：规避长连")
+                print (print_board(show_next))
+                onestep[x][y] = 0
+                i -= 1
+                continue
+            # else:
+            #     pass # 长连 continue
+            if check_win_all(onestep) == 1: 
+                print ("计算评估阶段：直接获胜")
+                onestep[x][y] = 0
+                return answer_board # 五连 return
+            # else:
+            #     pass
+            if double_4(x, y, show_next) == True: 
+                print ("计算评估阶段：规避4-4")
+                print (print_board(show_next))
+                onestep[x][y] = 0
+                i -= 1
+                continue # 4-4 continue
+            # else:
+            #     pass
+            if double_3(x, y, show_next) == True: 
+                print ("计算评估阶段：规避3-3")
+                print (print_board(show_next))
+                onestep[x][y] = 0
+                i -= 1
+                continue # 3-3 continue
+            # else:
+            #     pass
+        break
     return answer_board
 
 def find_max_values_white(prediction, board):
@@ -44,38 +77,88 @@ def find_max_values_white(prediction, board):
             continue
     return answer_board
 
+def machine_1_random(board):
+    answer_board = [[0]*15 for k in range(15)]
+    while True:
+        i = random.randint(0,14)
+        j = random.randint(0,14)
+        if board[i][j] == 0:
+            answer_board[i][j] = 1
+            break
+    return answer_board
+
 def machine_1(board, model1):
     return_board = [[0]*15 for i in range(15)]
-    one_step = board
+    one_step = copy.deepcopy(board)
     for x in range(15):
         for y in range(15):
             if board[x][y] == 0:
                 one_step[x][y] = 1
-                if check_win_all(one_step) == 1:
+                next_one_frame = show_board(one_step)
+                if long_forbidden(next_one_frame) == True: # 查6 - continue
+                    print ("搜索阶段：规避长连")
+                    print (print_board(next_one_frame))
+                    one_step[x][y] = 1
+                    continue
+                else:
+                    pass
+                if check_win_all(one_step) == 1: #查5
+                    print("搜索阶段：直接获胜")
                     return_board[x][y] = 1
                     one_step[x][y] = 0
                     return return_board
                 else:
                     one_step[x][y] = 0
-    prediction = model1.predict([divide_board(board)])
-    if np.argmax(prediction[0]) != 0:
-        answer_board = find_max_values_black(prediction, board)
+                break
+    prediction_black = model1.predict([divide_board(board)])
+    one_step = copy.deepcopy(board)
+    # print(np.argmax(prediction_black[0]))
+    # print(prediction_black[0])
+    if np.argmax(prediction_black[0]) != 0:
+        answer_board = find_max_values_black(prediction_black, board, one_step)
         return (answer_board)
     else:
         print("============黑棋：没有最大值=================")
+        print(prediction_black[0])
+        time.sleep(60)
+        one_step = copy.deepcopy(board)
         answer_board = [[0]*15 for k in range(15)]
         while True:
             i = random.randint(0,14)
             j = random.randint(0,14)
-            if board[i][j] == 0:
+            if board[i][j] == 0: 
                 answer_board[i][j] = 1
-                break
-        print (print_board(answer_board))
+                one_step[i][j] = 1
+                one_step_board = show_board(one_step)
+                if long_forbidden(one_step_board) == True: # 查6 - continue
+                    print("搜索阶段：规避长连")
+                    one_step[i][j] = 0
+                    answer_board[i][j] = 0
+                    continue
+                if check_win_all(one_step) == 1: #查5
+                    print("搜索阶段：直接获胜")
+                    return_board[x][y] = 1
+                    one_step[x][y] = 0
+                    return return_board
+                if double_4(x, y, one_step_board) == True: 
+                    print ("计算评估阶段：规避4-4")
+                    print (print_board(one_step_board))
+                    one_step[x][y] = 0
+                    answer_board[i][j] = 0
+                    continue # 4-4 continue
+                if double_3(x, y, one_step_board) == True: 
+                    print ("计算评估阶段：规避3-3")
+                    print (print_board(one_step_board))
+                    one_step[x][y] = 0
+                    answer_board[i][j] = 0
+                    continue
+                return answer_board
+            break
         return answer_board
 
 def machine_2(board, model2):
     return_board = [[0]*15 for i in range(15)]
-    one_step = board
+    one_step = copy.deepcopy(board)
     for x in range(15):
         for y in range(15):
             if board[x][y] == 0:
@@ -86,12 +169,14 @@ def machine_2(board, model2):
                     return return_board
                 else:
                     one_step[x][y] = 0
-    prediction = model2.predict([divide_board(board)])
-    if np.argmax(prediction[0]) != 0:
-        answer_board = find_max_values_white(prediction, board)
+    prediction_white = model2.predict([divide_board(board)])
+    if np.argmax(prediction_white[0]) != 0:
+        answer_board = find_max_values_white(prediction_white, board)
         return (answer_board)
     else:
         print("============白棋：没有最大值=================")
+        print(prediction_white[0])
+        time.sleep(60)
         answer_board = [[0]*15 for k in range(15)]
         while True:
             i = random.randint(0,14)
@@ -99,5 +184,14 @@ def machine_2(board, model2):
             if board[i][j] == 0:
                 answer_board[i][j] = -1
                 break
-        print (print_board(answer_board))
         return answer_board
+    
+def machine_2_random(board):
+    answer_board = [[0]*15 for k in range(15)]
+    while True:
+        i = random.randint(0,14)
+        j = random.randint(0,14)
+        if board[i][j] == 0:
+            answer_board[i][j] = -1
+            break
+    return answer_board
